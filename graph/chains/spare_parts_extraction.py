b8077e11b530a93533190987164179e2d6eb8509
AiQura <ahmedqura95@gmail.com>
2025-04-29 23:29:42 +0400

    Updating the websearch

--- graph/chains/spare_parts_extraction.py
+++ graph/chains/spare_parts_extraction.py
@@ -8,9 +8,10 @@ spare_parts_extraction_prompt = ChatPromptTemplate.from_messages(
         ("system", F"""You are a helpful expert Data analysis team lead . you will get an answer that may or may not contains Spare parts numbers, the answer contains alot of data so do the following . "
             "if the answer contains the spare parts numbers, you have to extract the parts names and the parts numbers along with the brand name and equipment type, the return has to be in a shape of question for the price. "
             "if the answer does not contain part numbers but contains brand name or type of equipment, return back '{SPARE_PARTS_EXTRACTION_END_ANSWER}'.
-            "the lenght has to be 350 charachter as MAX."
+            "the Question has to be a straight forward question without any saluting or other un-necessary words"
+            "the number of caracters in the question have to be 350 charachter as MAX. THIS IS A MUST."
 
-            [add the question here after rephrasing it as mentioned above]
+            Question: [add the question here after rephrasing it as mentioned above]
 
             """),
         MessagesPlaceholder(variable_name="messages")
--- graph/nodes/web_search.py
+++ graph/nodes/web_search.py
@@ -15,17 +15,19 @@ def web_search(state: GraphState) -> GraphState:
     if 'price_documents' not in state:
         state['price_documents'] = []
 
+    if 'error' in result_of_search:
+        raise result_of_search["error"]
+    
     for result in result_of_search["results"]:
-        state['price_documents'].append(f"{result['url']} : {result['content']}")
+        state['price_documents'].append(
+            f"{result['url']} : {result['content']}")
 
     prices_information = "\n\n".join(state['price_documents'])
 
     state["generation"] += f"\n\n\n\n{prices_information}"
 
-
     print(f"-------------------------------------> WEB SEARCH <-------------------------------------")
     print(prices_information)
     print("--------------------------------------------------------------------------")
-
 
     return state
